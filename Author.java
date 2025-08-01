7560f56bf90463332b73bf62c2622f07d37bd92e
abramovanv <38113082+abramovanv@users.noreply.github.com>
2025-08-01 07:04:30 +0300

    Add files via upload

--- Author.java
+++ Author.java
@@ -5,7 +5,7 @@ public class Author {
 
     public Author(String name, String surname, int rating) {
         this.name = name;
-        this.surname=surname;
+        this.surname = surname;
         this.rating = rating;
     }
 
--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Author author = new  Author("Name", "Petrov", 500);
+        Author author = new Author("Name", "Petrov", 500);
         Book book = new Book("Название книги", 2025, author, 1);
 
         System.out.println(book.isBig());
@@ -10,8 +10,5 @@ public class Main {
         System.out.println(book.matches("книги"));
         System.out.println(book.matches("Petr"));
         System.out.println(book.estimatePrice());
-
-
-
     }
 }
